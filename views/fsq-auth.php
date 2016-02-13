391258049da1e57f28e1cc764264fe6b01187ae5
felix schwenzel <dipl.ix@gmail.com>
2016-02-13 08:32:56 +0100

    connect to foursquare

---
+++ views/fsq-auth.php
@@ -0,0 +1,6 @@
+<h1>Congrats, you successfully signed in!</h1>
+
+<p>Now you need to connect your Foursquare account</p>
+
+<a class="btn btn-primary" href="/auth/fsq-start">Connect Foursquare</a>
+
