c40754784cbc0265cf341d153a16008c1abfadd6
felix schwenzel <dipl.ix@gmail.com>
2016-02-13 08:45:44 +0100

    some small changes

--- scripts/fill.php
+++ scripts/fill.php
@@ -5,29 +5,29 @@ require 'lib/Savant.php';
 require 'lib/config.php';
 require 'lib/helpers.php';
 require 'lib/markdown.php';
-require 'lib/instagram.php';
+require 'lib/fsq.php';
 
 $users = ORM::for_table('users')
-  ->where_not_null('last_instagram_photo')
+  ->where_not_null('last_fsq_checkin')
   ->where_not_null('last_micropub_url')
-  ->where_null('last_instagram_img_url')
+  ->where_null('last_fsq_img_url')
   ->find_many();
 foreach($users as $user) {
   echo $user->url . "\n";
   try {
-    $photo = IG\get_photo($user, $user->last_instagram_photo);
-    $public = IG\user_is_public($user);
+    $checkin = FSQ\get_checkin($user, $user->last_fsq_checkin);
+    $public = FSQ\user_is_public($user);
 
-    $user->ig_public = $public ? 1 : 0;
-    if($photo) {
-      $user->last_instagram_img_url = $photo->images->standard_resolution->url;
-      echo $photo->images->standard_resolution->url . "\n";
+    $user->fsq_public = $public ? 1 : 0;
+    if($checkin) {
+      $user->last_fsq_img_url = $checkin->images->standard_resolution->url;
+      echo $checkin->images->standard_resolution->url . "\n";
     } else {
-      echo "no photo found\n";
+      echo "no checkin found\n";
     }
     $user->save();
   } catch(Exception $e) {
-    echo "Invalid instagram token\n";
+    echo "Invalid Foursquare token\n";
   }
 }
 
