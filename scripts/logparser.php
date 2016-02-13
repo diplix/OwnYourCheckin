49d231deed40c84ece1aecb23d7da61dce349364
felix schwenzel <dipl.ix@gmail.com>
2016-02-13 08:48:10 +0100

    small changes

--- scripts/logparser.php
+++ scripts/logparser.php
@@ -5,7 +5,7 @@ require 'lib/Savant.php';
 require 'lib/config.php';
 require 'lib/helpers.php';
 require 'lib/markdown.php';
-require 'lib/instagram.php';
+require 'lib/fsq.php';
 
 $log = file_get_contents('scripts/worker.log');
 
@@ -16,22 +16,22 @@ foreach($entries as $entry) {
   if(preg_match('/\[object_id\] => (\d+)/', $entry, $match)) {
     $user_id = $match[1];
 
-    echo "Instagram user_id: $user_id\n";
+    echo "Foursquare user_id: $user_id\n";
 
-    $user = ORM::for_table('users')->where('instagram_user_id', $user_id)->find_one();
+    $user = ORM::for_table('users')->where('fsq_user_id', $user_id)->find_one();
     if($user) {
 
       if(preg_match('/Location: (.+)/', $entry, $match)) {
         $micropub_location = $match[1];
-        echo "Found photo: $micropub_location\n";
+        echo "Found checkin: $micropub_location\n";
 
         $user->last_micropub_url = $micropub_location;
-        $user->photo_count = $user->photo_count + 1;
+        $user->checkin_count = $user->checkin_count + 1;
 
         if(preg_match('/\[published\] => (.+)/', $entry, $match)) {
           $published = strtotime($match[1]);
           if($published > time() - (86400*7)) {
-            $user->photo_count_this_week = $user->photo_count_this_week + 1;
+            $user->checkin_count_this_week = $user->checkin_count_this_week + 1;
           }
         }
 
@@ -39,7 +39,7 @@ foreach($entries as $entry) {
       }
 
     } else {
-      echo "OwnYourGram account not found\n";
+      echo "OwnYourCheckin account not found\n";
     }
 
   }
